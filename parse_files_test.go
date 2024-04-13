492a9685e3637e9d082e31f63f217f217252d3e9
John Costa <johncosta027@gmail.com>
2024-04-13 17:14:45 +0100

    REFACTOR: Removing package level generation test

--- parse_files_test.go
+++ parse_files_test.go
@@ -2,35 +2,7 @@ package main
 
 import "testing"
 
-func TestGoFile(t *testing.T) {
-	valibotString, err := ParseV2("./test/test1/a.go")
-
-	valibotValidator := `
-import { object, string } from 'valibot';
-
-const A = object({
-  Hello: string(),
-});
-
-const B = object({
-  Hello: string(),
-});
-`
-	if err != nil {
-		t.Log(err)
-		t.FailNow()
-	}
-
-	t.Log(valibotString)
-
-	if valibotString != valibotValidator {
-		t.FailNow()
-	}
-}
-
 func TestNestedDeps(t *testing.T) {
-	t.Skip()
-
 	valibotString, err := ParseV2("./test/test2/a.go")
 
 	valibotValidator := `
